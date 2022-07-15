74625acae806502ff89c15a443fc212b1e3b9671
viniihds <viniciushsilva2210@gmail.com>
2022-07-15 14:37:19 -0300

    aula do joao 2

---
+++ auladojoao/front/aula-joao/src/views/HomeView.vue
@@ -0,0 +1,29 @@
+<script>
+import axios from "axios";
+
+
+export default {
+  data() {
+    return{
+      listaDeSabores: [],
+    };
+  },
+  onMounted(){
+    this.getSabores();
+},
+methods:{
+  async getSabores(){
+      let res = axios.get("http://localhost:8087/sabores");
+      this.listaDeSabores = (await res).data;
+    },
+  }
+}
+
+</script>
+
+<template>
+  <button @click="getSabores()">Pegar Sabores</button>
+  <div>
+    <h1 v-for="sabor in listaDeSabores" :key="sabor">{{ sabor }}</h1>
+  </div>
+</template>
